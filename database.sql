b75d939201d3424c5629e203b1a43ab09014a619
Luize Tisse <luizetereza.tisse@gmail.com>
2026-06-08 16:21:08 -0300

    Update URLs sql

--- database.sql
+++ database.sql
@@ -96,20 +96,39 @@ INSERT INTO categorias (nome) VALUES
     ('Infantil'),
     ('Documentário');
 
-INSERT INTO filmes (titulo, descricao, tipo, categoria_id) VALUES
-    ('Matrix',                          'Filme sobre realidade simulada',          'filme', 6),
-    ('Interestelar',                    'Viagem espacial',                         'filme', 6),
-    ('Vingadores',                      'Heróis da Marvel',                        'filme', 1),
-    ('Ta Chovendo Hambúrguer',          'Chove Hambúrguer',                        'filme', 3),
-    ('Smurfs',                          'Um filme sobre Smurfs',                   'filme', 7),
-    ('A Fantástica Fábrica de Chocolate','Battle Royale Infantil por herança',     'filme', 2),
-    ('Perdido pra Cachorro',            'POV de Chiuaua',                          'filme', 4),
-    ('Carros 2',                        'Carros espiões',                          'filme', 3),
-    ('Internet: O Filme',               'Youtubers',                               'filme', 4),
-    ('A Nova Onda do Imperador',        'Lhama',                                   'filme', 3);
+INSERT INTO filmes (titulo, descricao, tipo, categoria_id, capa) VALUES
+    ('Matrix',                          'Filme sobre realidade simulada',          'filme', 6, "https://static.wikia.nocookie.net/dublagem/images/7/7a/Matrix.jpg/revision/latest?cb=20250716174206&path-prefix=pt-br"),
+    ('Interestelar',                    'Viagem espacial',                         'filme', 6, "http://papodecinema.com.br/wp-content/uploads/2014/10/20141001-interestelar-papo-de-cinema.webp"),
+    ('Vingadores',                      'Heróis da Marvel',                        'filme', 1, "https://www.papodecinema.com.br/wp-content/uploads/2019/02/20190422-mryl2_003a_g_por-br_64x94-204x300.webp"),
+    ('Ta Chovendo Hambúrguer',          'Chove Hambúrguer',                        'filme', 3, "https://www.papodecinema.com.br/wp-content/uploads/2012/08/20200507-ta-chovendo-hamburguer-papo-de-cinema-cartaz-209x300.webp"),
+    ('Smurfs',                          'Um filme sobre Smurfs',                   'filme', 7, "https://www.papodecinema.com.br/wp-content/uploads/2012/05/20190423-poster-pequeno-imp-couche-a3-do-filme-os-smurfs-ver-3-d_nq_np_14053-mlb196974927_5029-f-204x300.webp"),
+    ('A Fantástica Fábrica de Chocolate','Battle Royale Infantil por herança',     'filme', 2, "https://www.papodecinema.com.br/wp-content/uploads/2014/06/20240103-a-fantastica-fabrica-de-chocolate-2005-papo-de-cinema-cartaz-222x300.webp"),
+    ('Perdido pra Cachorro',            'POV de Chiuaua',                          'filme', 4, "https://www.papodecinema.com.br/wp-content/uploads/2012/12/20200807-perdido-pra-cachorro-papo-de-cinema-cartaz-207x300.webp"),
+    ('Carros 2',                        'Carros espiões',                          'filme', 3, "https://static.wikia.nocookie.net/dublagem/images/a/af/Carros_2.jpeg/revision/latest?cb=20230721135632&path-prefix=pt-br"),
+    ('Internet: O Filme',               'Youtubers',                               'filme', 4, "https://media.themoviedb.org/t/p/w300_and_h450_face/7kyQPkPj6YDYzX4oMymtoiTzLx2.jpg"),
+    ('A Nova Onda do Imperador',        'Lhama',                                   'filme', 3, "https://www.aabbportoalegre.com.br/intranet/modulos/biblioteca/imgs/1896.jpg");
+
+
 
 -- Admin de teste
 -- CPF: 00000000000  /  Senha: 123  (hash bcrypt válido)
 INSERT INTO usuarios (nome, cpf, data_nascimento, email, senha, role) VALUES
     ('Administrador', '00000000000', '1990-01-01', 'admin@algoflix.com',
-     '$2b$10$CS.Gx6K7MW7p82S2gcfm.exzhwYLwS3TfCx0ShCMjmKU.3T7Grrda', 'admin');
+     '$2b$10$CS.Gx6K7MW7p82S2gcfm.exzhwYLwS3TfCx0ShCMjmKU.3T7Grrda', 'admin'),
+    ('Maria Silva',   '11111111111', '1995-05-20', 'maria@algoflix.com',
+     '$2b$10$CS.Gx6K7MW7p82S2gcfm.exzhwYLwS3TfCx0ShCMjmKU.3T7Grrda', 'usuario');
+
+
+-- Avaliações de teste (usuario_id, filme_id, nota)
+-- Admin = 1, Maria = 2
+INSERT INTO avaliacoes (usuario_id, filme_id, nota) VALUES
+    (1,  1, 5),  (2,  1, 5),  -- Matrix: 5.0
+    (1,  2, 5),  (2,  2, 4),  -- Interestelar: 4.5
+    (1,  3, 4),  (2,  3, 4),  -- Vingadores: 4.0
+    (1,  4, 3),  (2,  4, 4),  -- Ta Chovendo Hambúrguer: 3.5
+    (1,  5, 2),  (2,  5, 3),  -- Smurfs: 2.5
+    (1,  6, 5),                -- A Fantástica Fábrica de Chocolate: 5.0
+    (1,  7, 3),                -- Perdido pra Cachorro: 3.0
+    (1,  8, 4),  (2,  8, 3),  -- Carros 2: 3.5
+    (2,  9, 2),                -- Internet: O Filme: 2.0
+    (1, 10, 4),  (2, 10, 5);  -- A Nova Onda do Imperador: 4.5
